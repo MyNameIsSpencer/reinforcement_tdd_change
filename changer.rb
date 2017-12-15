12ef7a5f04d7bc969619287eec20c2d61107a172
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-12-15 17:53:27 -0500

    its done kinda but whatever

--- changer.rb
+++ changer.rb
@@ -1,26 +1,56 @@
-class Changer
+# class Changer
+#   @@change = []
+#
+#   def initialize(purse)
+#     @purse = purse
+#   end
+#
+#   def purse
+#     @purse
+#   end
+#
+#   def make_change(purse)
+#     cents2 = countdown(cents, 25)
+#     cents3 = countdown(cents2, 10)
+#     cents4 = countdown(cents3, 5)
+#     cents5 = countdown(cents4, 1)
+#
+#     return @@change
+#   end
+#
+#   def countdown(cents, coin)
+#     pennies = cents
+#     until coin > cents
+#       @@change << coin
+#       pennies -= coin
+#     end
+#     pennies
+#   end
+#
+# end
 
+
+class Changer
   def self.make_change(cents)
     change = []
 
-    while cents > 0 do
-      if cents >= 25
-        change << 25
-        cents -= 25
-      elsif cents >= 10
-        change << 10
-        cents -= 10
-      elsif cents >= 5
-        change << 5
-        cents -= 5
-      elsif cents >= 1
-        change << 1
-        cents -= 1
-      end
+    until cents < 25 do
+      change << 25
+      cents -= 25
+    end
+    until cents < 10
+      change << 10
+      cents -= 10
+    end
+    until cents < 5
+      change << 5
+      cents -= 5
+    end
+    until cents < 1
+      change << 1
+      cents -= 1
     end
     change
   end
-
-
 
 end
